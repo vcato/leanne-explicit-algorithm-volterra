b10b29d219f9750e379382051960ca736050f8d0
Leann Dong <jdleanne@gmail.com>
2019-10-17 01:00:24 +1100

    added a better looking table

--- h_sumt.cpp
+++ h_sumt.cpp
@@ -15,7 +15,7 @@ double rect(double t, double low, double up);
 double impul(double t);
 double my_fct(double t, double a, double k);
 double series(int n, double t);
-double h_series(double k, double tt, double t);
+//double h_series(double k, double tt, double t);
 
 // Function definition
 int nCr(int n, int r) 
@@ -70,26 +70,31 @@ double series(int n, double t){
 // Driver code 
 int main() 
 { 
- double t, tt, k; 
- double h;
+double t, tt, k; 
+double h;
  //h = static_cast<float>(t)/nn;
-    cout <<" Enter the value for t: ";
-    cin >> t;
- //   cout <<" Enter the value for n (n>=1): ";
-    cout <<" Enter the value for T (T>=1): ";
-    cin >> tt;
-    cout <<" Enter the value for k: ";
-    cin >> k;
-   // cout << h_series(0.3,2.9,0.8) << endl;
-   // cout << h_series(0.3,3.567,0.8) << endl;
-   // cout << h_series(0.3,4.788,0.8) << endl;
-    double sum = 0.0;
+cout <<" Enter the value for t: ";
+cin >> t;
+//   cout <<" Enter the value for n (n>=1): ";
+cout <<" Enter the value for T (T>=1): ";
+cin >> tt;
+cout <<" Enter the value for k: ";
+cin >> k;
+
+
+    cout  << "           n     gamma_n       h_n";
+ //   std::cout << "         l1       l2" << endl;
+ //   std::cout << "                 answer1      answer1";
+    cout << "  " << "  " << endl << endl;
+
+    cout << fixed << setprecision(6);
+
+double sum = 0.0;
 
  for (int i=1; i<floor(tt)+1; i++ ){
     sum +=  pow(k,i)*series(i,t);
-    cout << setw(12) << i << setw(13) << series(i,t) << setw(13) << pow(k,i) <<setw(13) << series(i,t)*pow(k,i)<< setw(13)<< sum;
-    //cout << setw(12);
-    //series(n,h*i);
+//    cout << setw(12) << i << setw(13) << series(i,t) << setw(13) << pow(k,i) <<setw(13) << series(i,t)*pow(k,i)<< setw(13)<< sum;
+    cout << setw(12) << i << setw(13) << series(i,t) << setw(13) << setw(13)<< sum;
     cout << endl;
  }
 
@@ -97,16 +102,18 @@ int main()
 } 
 
 /* 
-leanne@leanne-Latitude-5480:~/explicit-algorithm-volterra$ ./htest
+leanne@leanne-HP-ProBook-450-G5:~/explicit-algorithm-volterra$ ./testh
  Enter the value for t: 0.8
  Enter the value for T (T>=1): 7.3
  Enter the value for k: 0.7
-           1            1          0.7          0.7          0.7
-           2          0.8         0.49        0.392        1.092
-           3         0.32        0.343      0.10976      1.20176
-           4    0.0853333       0.2401    0.0204885      1.22225
-           5    0.0170667      0.16807   0.00286839      1.22512
-           6   0.00273067     0.117649   0.00032126      1.22544
-           7  0.000364089    0.0823543  2.99843e-05      1.22547
+           n     gamma_n       h_n    
+
+           1     1.000000     0.700000
+           2     0.800000     1.092000
+           3     0.320000     1.201760
+           4     0.085333     1.222249
+           5     0.017067     1.225117
+           6     0.002731     1.225438
+           7     0.000364     1.225468
 
 */
